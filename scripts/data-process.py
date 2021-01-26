8f02a9b1d2e716a254dfae32fb49e9d4e41ad2a4
Diego Vieira <dgavieira@users.noreply.github.com>
2021-01-26 15:32:38 -0400

    Create data-process.py
    métodos:
    acrescenta_classificacao - transforma pos e neg para 1 e 0
    
    remove_stopwords
    remove_tags
    remove_pontuacao

---
+++ scripts/data-process.py
@@ -0,0 +1,60 @@
+from string import punctuation
+
+import nltk
+from nltk import *
+
+
+def acrescenta_classificacao(df):
+    classificacao = df['sentiment'].replace(['negative', 'positive'], [0, 1])
+    df['classificacao'] = classificacao
+    return df
+
+
+def remove_stopwords(df):
+    palavras_irrelevantes = nltk.corpus.stopwords.words('english')
+    frase_processada = list()
+    token_espaco = tokenize.WhitespaceTokenizer()
+
+    for opiniao in df['review']:
+        nova_frase = list()
+        palavras_texto = token_espaco.tokenize(opiniao)
+        for palavra in palavras_texto:
+            if palavra not in palavras_irrelevantes:
+                nova_frase.append(palavra)
+        frase_processada.append(' '.join(nova_frase))
+
+    df['stopwords'] = frase_processada
+
+    return df
+
+
+def remove_tags(df):
+    df['stopwords'] = df['stopwords'].str.replace(r'<br />', '')
+    df['stopwords'] = df['stopwords'].str.replace(r'\'style\'', '')
+
+    return df
+
+
+def remove_pontuacao(df):
+    palavras_irrelevantes = nltk.corpus.stopwords.words('english')
+    token_pontuacao = tokenize.WordPunctTokenizer()
+    pontuacao = list()
+    for ponto in punctuation:
+        pontuacao.append(ponto)
+
+    pontuacao_stopwords = pontuacao + palavras_irrelevantes
+    frase_processada = list()
+    for opiniao in df['stopwords']:
+        nova_frase = list()
+        palavras_texto = token_pontuacao.tokenize(opiniao)
+        for palavra in palavras_texto:
+            if palavra not in pontuacao_stopwords:
+                nova_frase.append(palavra)
+        frase_processada.append(' '.join(nova_frase))
+
+    df['punctuation'] = frase_processada
+    df['punctuation'] = df['punctuation'].str.replace(r' .. ', ' ')
+    df['punctuation'] = df['punctuation'].str.replace(r' ... ', ' ')
+    df['punctuation'] = df['punctuation'].str.replace(r' .... ', ' ')
+
+    return df
